e2097567c726b7c86044b2a6b155f4d240dd8d48
Golovko Nikolay <dakuenjery@gmail.com>
2016-10-13 01:09:17 +0300

    no message

--- Assets/_scripts/DataStructs.cs
+++ Assets/_scripts/DataStructs.cs
@@ -3,60 +3,69 @@ using System.Runtime.InteropServices;
 
 namespace DataSt
 {
-  
-  [StructLayout(LayoutKind.Sequential, Size = 282)]
-  public struct Data
-  {
 
-    [MarshalAs(UnmanagedType.U1)]
-    public byte typeTrain;
+	[StructLayout(LayoutKind.Sequential, Pack = 1, Size = 282)]
+	public struct Data
+	{
 
-    [MarshalAs(UnmanagedType.LPStr, SizeConst = 256)]
-    public string nameRailRoad;
+		[MarshalAs(UnmanagedType.U1)]
+		public byte typeTrain;
 
-    [MarshalAs(UnmanagedType.R8)]
-    public double modelTime;
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+		public string nameRailRoad;
 
-    [MarshalAs(UnmanagedType.R8)]
-    public double coordinate;
+		[MarshalAs(UnmanagedType.R8)]
+		public double modelTime;
 
-    [MarshalAs(UnmanagedType.R8)]
-    public double currentSpeed;
+		[MarshalAs(UnmanagedType.R8)]
+		public double coordinate;
 
-    [MarshalAs(UnmanagedType.I1)]
-    public sbyte direction;
+		[MarshalAs(UnmanagedType.R8)]
+		public double currentSpeed;
 
-
-    public static byte[] Serialize(Data data)
-    {
-      int rawSize = Marshal.SizeOf(typeof(Data));
-      IntPtr buffer = Marshal.AllocHGlobal(rawSize);
-      Marshal.StructureToPtr(data, buffer, false);
-      byte[] rawData = new byte[rawSize];
-      Marshal.Copy(buffer,rawData,0,rawSize);
-      Marshal.FreeHGlobal(buffer); //Освобождает память, выделенную ранее из неуправляемой памяти процесса.
-      return rawData;
-    }
-
-    public static Data Deserialize(byte[] rawData)
-    {
-      int rawsize = Marshal.SizeOf(typeof(Data));
-      if(rawsize > rawData.Length)
-        return default(Data);
-
-      IntPtr buffer = Marshal.AllocHGlobal(rawsize);
-      Marshal.Copy(rawData, 0, buffer, rawsize);
-      Data obj = (Data)Marshal.PtrToStructure(buffer, typeof(Data));
-      Marshal.FreeHGlobal(buffer); //Освобождает память, выделенную ранее из неуправляемой памяти процесса.
-      return obj;
-    }
+		[MarshalAs(UnmanagedType.I1)]
+		public sbyte direction;
 
 
-    public override string ToString()
-    {
-      string a = typeTrain.ToString() + " " + nameRailRoad.ToString() + " " + modelTime.ToString() + " " +
-                 coordinate.ToString() + " " + currentSpeed.ToString();
-      return a;
-    }
-  }
+		public static byte[] Serialize(Data data)
+		{
+			int rawSize = Marshal.SizeOf(typeof(Data));
+			IntPtr buffer = Marshal.AllocHGlobal(rawSize);
+			Marshal.StructureToPtr(data, buffer, false);
+			byte[] rawData = new byte[rawSize];
+			Marshal.Copy(buffer, rawData, 0, rawSize);
+			Marshal.FreeHGlobal(buffer); //Освобождает память, выделенную ранее из неуправляемой памяти процесса.
+			return rawData;
+		}
+
+		public static Data Deserialize(byte[] rawData)
+		{
+			int rawsize = Marshal.SizeOf(typeof(Data));
+			if (rawsize > rawData.Length)
+				return default(Data);
+
+			Data obj = default(Data);
+			IntPtr buffer = Marshal.AllocHGlobal(rawsize);
+
+			try
+			{
+				Marshal.Copy(rawData, 0, buffer, rawsize);
+				obj = (Data)Marshal.PtrToStructure(buffer, typeof(Data));
+			}
+			catch (Exception ex)
+			{
+				var errstring = ex.ToString();
+			}
+			finally
+			{
+				Marshal.FreeHGlobal(buffer); //Освобождает память, выделенную ранее из неуправляемой памяти процесса.
+			}
+			return obj;
+		}
+
+		public override string ToString()
+		{
+			return string.Format("[Data: typeTrain={0}, nameRailRoad={1}, modelTime={2}, coordinate={3}, currentSpeed={4}, direction={5}]", typeTrain, nameRailRoad, modelTime, coordinate, currentSpeed, direction);
+		}
+	}
 }
--- Assets/_scripts/GameManager.cs
+++ Assets/_scripts/GameManager.cs
@@ -11,71 +11,68 @@ using UnityEditor.VersionControl;
 
 public class GameManager : MonoBehaviour
 {
-  private ServerUnity server;
+	private ServerUnity server;
 
-  void Start()
-  {
+	void Start()
+	{
 
-    //Data _d = new Data();
-    //_d.currentSpeed = 91;
-    //_d.coordinate = 1;
-    //_d.nameRailRoad = "asd";
+		//Data _d = new Data();
+		//_d.currentSpeed = 91;
+		//_d.coordinate = 1;
+		//_d.nameRailRoad = "asd";
 
-    //var t = Data.Serialize(_d);
-    //Debug.Log(t);
+		//var t = Data.Serialize(_d);
+		//Debug.Log(t);
 
-    //var tt = Data.Deserialize(t);
-    //Debug.Log(tt);
+		//var tt = Data.Deserialize(t);
+		//Debug.Log(tt);
 
-    server = new ServerUnity();
-    server.StartServer();
-  }
+		server = new ServerUnity();
+		server.StartServer();
+	}
 }
 
 public class ServerUnity
 {
-  byte[] buffer = new byte[512];
+	byte[] buffer = new byte[512];
 
-  public void StartServer()
-  {
-    try
-    {
-      IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8181);
+	public void StartServer()
+	{
+		try {
+			IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8181);
 
-    var tcpListener = new TcpListener(ipEndPoint);
-    tcpListener.Start();
-    tcpListener.BeginAcceptSocket(AsyncAcceptSocket, tcpListener);
+			var tcpListener = new TcpListener(ipEndPoint);
+			tcpListener.Start();
+			tcpListener.BeginAcceptSocket(AsyncAcceptSocket, tcpListener);
+
+			Debug.Log("Start listen");
     
-    }
-    catch(Exception e)
-    {
-      Debug.LogError(e.ToString());
-    }
-    finally
-    {
-    }
-  }
+		} catch (Exception e) {
+			Debug.LogError(e.ToString());
+		} finally {
+		}
+	}
 
-  private void AsyncAcceptSocket(IAsyncResult ar)
-  {
-    var tcpListener = (TcpListener) ar.AsyncState;
-    var socket = tcpListener.EndAcceptSocket(ar);
+	private void AsyncAcceptSocket(IAsyncResult ar)
+	{
+		var tcpListener = (TcpListener)ar.AsyncState;
+		var socket = tcpListener.EndAcceptSocket(ar);
 
-    socket.NoDelay = true;
+		socket.NoDelay = true;
 
-    socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, AsyncRecieve, socket);
+		socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, AsyncRecieve, socket);
 
-    Debug.Log("AsyncAcceptSocket");
-  }
+		Debug.Log("AsyncAcceptSocket");
+	}
 
-  private void AsyncRecieve(IAsyncResult ar)
-  {
-    var socket = (Socket) ar.AsyncState;
-    var byteCount = socket.EndReceive(ar);
-    var msg = Data.Deserialize(buffer);
-    Debug.Log(msg);
-    Debug.Log("AsyncRecieve " + byteCount);
+	private void AsyncRecieve(IAsyncResult ar)
+	{
+		var socket = (Socket)ar.AsyncState;
+		var byteCount = socket.EndReceive(ar);
+		var msg = Data.Deserialize(buffer);
+		Debug.Log(msg.ToString());
+		Debug.Log("AsyncRecieve " + byteCount);
 
-    socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, AsyncRecieve, socket);
-  }
+		socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, AsyncRecieve, socket);
+	}
 }

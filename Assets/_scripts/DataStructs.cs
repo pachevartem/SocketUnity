1ddf9997472e9f17e96528d5f9ac0dc9c4c58964
pachevArtem <pachevartem@ya.ru>
2016-10-12 19:22:02 +0300

    Kolya davai

---
+++ Assets/_scripts/DataStructs.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DataSt
+{
+  
+  [StructLayout(LayoutKind.Sequential, Size = 282)]
+  public struct Data
+  {
+
+    [MarshalAs(UnmanagedType.U1)]
+    public byte typeTrain;
+
+    [MarshalAs(UnmanagedType.LPStr, SizeConst = 256)]
+    public string nameRailRoad;
+
+    [MarshalAs(UnmanagedType.R8)]
+    public double modelTime;
+
+    [MarshalAs(UnmanagedType.R8)]
+    public double coordinate;
+
+    [MarshalAs(UnmanagedType.R8)]
+    public double currentSpeed;
+
+    [MarshalAs(UnmanagedType.I1)]
+    public sbyte direction;
+
+
+    public static byte[] Serialize(Data data)
+    {
+      int rawSize = Marshal.SizeOf(typeof(Data));
+      IntPtr buffer = Marshal.AllocHGlobal(rawSize);
+      Marshal.StructureToPtr(data, buffer, false);
+      byte[] rawData = new byte[rawSize];
+      Marshal.Copy(buffer,rawData,0,rawSize);
+      Marshal.FreeHGlobal(buffer); //Освобождает память, выделенную ранее из неуправляемой памяти процесса.
+      return rawData;
+    }
+
+    public static Data Deserialize(byte[] rawData)
+    {
+      int rawsize = Marshal.SizeOf(typeof(Data));
+      if(rawsize > rawData.Length)
+        return default(Data);
+
+      IntPtr buffer = Marshal.AllocHGlobal(rawsize);
+      Marshal.Copy(rawData, 0, buffer, rawsize);
+      Data obj = (Data)Marshal.PtrToStructure(buffer, typeof(Data));
+      Marshal.FreeHGlobal(buffer); //Освобождает память, выделенную ранее из неуправляемой памяти процесса.
+      return obj;
+    }
+
+
+    public override string ToString()
+    {
+      string a = typeTrain.ToString() + " " + nameRailRoad.ToString() + " " + modelTime.ToString() + " " +
+                 coordinate.ToString() + " " + currentSpeed.ToString();
+      return a;
+    }
+  }
+}
--- Assets/_scripts/GameManager.cs
+++ Assets/_scripts/GameManager.cs
@@ -4,140 +4,78 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System;
+using System.Runtime.InteropServices;
 using System.Text;
+using DataSt;
+using UnityEditor.VersionControl;
 
 public class GameManager : MonoBehaviour
 {
-    private Thread _server;
-    void Start()
-    {
-        _server = new Thread(StartServer);
-        _server.Start();
-    }
+  private ServerUnity server;
 
-    void StartServer()
-    {
-        ServerUnity _server = new ServerUnity();
-        _server.StartSrever();
-    }
+  void Start()
+  {
 
+    //Data _d = new Data();
+    //_d.currentSpeed = 91;
+    //_d.coordinate = 1;
+    //_d.nameRailRoad = "asd";
 
-    public void OnDestroy()
-    {
-        _server.Abort();
-    }
+    //var t = Data.Serialize(_d);
+    //Debug.Log(t);
 
-   
+    //var tt = Data.Deserialize(t);
+    //Debug.Log(tt);
 
+    server = new ServerUnity();
+    server.StartServer();
+  }
 }
-
 
 public class ServerUnity
 {
-    ~ServerUnity()
+  byte[] buffer = new byte[512];
+
+  public void StartServer()
+  {
+    try
     {
-        handler.Shutdown(SocketShutdown.Both);
-        handler.Close();
-        Debug.LogError("Соккет зaкрыт");
+      IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8181);
+
+    var tcpListener = new TcpListener(ipEndPoint);
+    tcpListener.Start();
+    tcpListener.BeginAcceptSocket(AsyncAcceptSocket, tcpListener);
+    
     }
-
-    private Socket handler;
-
-    public void StartSrever()
+    catch(Exception e)
     {
-        IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8181);
-        Socket sListerner = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-        try
-        {
-            sListerner.Bind(ipEndPoint);
-            sListerner.Listen(100);
-            while(true)
-            {
-                Debug.LogError("Начинаем слушать сокет: " + ipEndPoint);
-                handler = sListerner.Accept();
-                string data = null;
-                var bytes = new byte[1024];
-                var byteRec = handler.Receive(bytes);
-                data += Encoding.UTF8.GetString(bytes, 0, byteRec);
-                Debug.LogError("Полученный текст от клиента: " + data);
-                string Answer;
-                float Speed = StringChangeFloat(data);
-                if(Speed == -9999)
-                {
-                    Answer = "Pls write only Speed Value, dont use Words";
-                }
-                else
-                {
-                    Answer = "Thanks, train speed is - " + Speed.ToString();
-                    TrainController.Speed = Speed;
-                }
-
-                byte[] msg = Encoding.UTF8.GetBytes(Answer);
-                handler.Send(msg);
-                //handler.Shutdown(SocketShutdown.Both);
-                //handler.Close();
-                //Debug.LogError("Соккет зaкрыт");
-            }
-        }
-        catch(Exception e)
-        {
-            Debug.LogError(e.ToString());
-        }
-        finally
-        {
-            Debug.LogError("У тебя ошибка - фикси и ищи");
-            StartSrever();
-        }
+      Debug.LogError(e.ToString());
     }
-
-    /// <summary>
-    /// Передать строку, и величену знаков (пример: скрость до 999, значит значение capasiteSpeed = 3)
-    /// </summary>
-    /// <param name="_str"></param>
-    /// <param name="capasitySpeed"></param>
-    /// <returns></returns>
-    int StringChange(string _str, int capasitySpeed)
+    finally
     {
-        string _change = _str.Replace(" ", string.Empty);
-
-        if(_change[0] == '-')
-        {
-            if(_change.Length > capasitySpeed + 1)
-            {
-                _change = _change.Remove(capasitySpeed + 1);
-            }
-        }
-        else
-        {
-            if(_change.Length > capasitySpeed)
-            {
-                _change = _change.Remove(capasitySpeed);
-            }
-        }
-
-
-        int result = -99;
-        bool isInt = Int32.TryParse(_change, out result);
-        //Debug.Log(isInt);
-
-
-        return result = isInt ? result : -9999;
     }
+  }
 
-    float StringChangeFloat(string _str)
-    {
-        _str = _str.Replace(" ", string.Empty);
+  private void AsyncAcceptSocket(IAsyncResult ar)
+  {
+    var tcpListener = (TcpListener) ar.AsyncState;
+    var socket = tcpListener.EndAcceptSocket(ar);
 
-        float result;
-        bool isFloat = float.TryParse(_str, out result);
+    socket.NoDelay = true;
 
-        if(isFloat && _str.Length > 6)
-        {
-            _str.Remove(6);
-        }
-        return result = isFloat ? result : -9999;
-    }
+    socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, AsyncRecieve, socket);
 
+    Debug.Log("AsyncAcceptSocket");
+  }
 
+  private void AsyncRecieve(IAsyncResult ar)
+  {
+    var socket = (Socket) ar.AsyncState;
+    var byteCount = socket.EndReceive(ar);
+    var msg = Data.Deserialize(buffer);
+    Debug.Log(msg);
+    Debug.Log("AsyncRecieve " + byteCount);
+
+    socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, AsyncRecieve, socket);
+  }
 }

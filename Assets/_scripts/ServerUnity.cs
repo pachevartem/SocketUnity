657addea17f7ebbee35123a750aa32faaf7f9f99
Pachev Artem <pachevartem@ya.ru>
2016-10-18 18:29:56 +0300

    Add coment

--- Assets/_scripts/ServerUnity.cs
+++ Assets/_scripts/ServerUnity.cs
@@ -1,74 +1,98 @@
-﻿using SocketClient;
+﻿
 using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 
+/// <summary>
+/// Класс описывающий соединение с сервером
+/// </summary>
 public class ServerUnity : MonoBehaviour
 {
 
-  Thread StartRequest;
-  private SocketClientProgram client;
-  
-  void Start()
-  {
-    client = new SocketClientProgram();
-    StartRequest = new Thread(client.Start);
-    StartRequest.Start();
-  }
+	/// <summary>
+	/// Создание нового потока
+	/// </summary>
+	Thread StartRequest;
 
-  ~ServerUnity()
-  {
-    StartRequest.Abort();
-    Debug.Log("Killall");
-  }
+	/// <summary>
+	/// Ссылка на класс осуществляющий соединение с сервером
+	/// </summary>
+	private SocketClientProgram client;
+
+
+	/// <summary>
+	/// Метод зарезирвированый UNITY3D выполняется, при запуске программы
+	/// </summary>
+	void Start ()
+	{
+		client = new SocketClientProgram ();
+		StartRequest = new Thread (client.Start);
+		StartRequest.Start ();
+	}
+	/// <summary>
+	/// Деструктор класса, в нем принудительно выключается созданный поток.
+	/// </summary>
+	~ServerUnity ()
+	{
+		StartRequest.Abort ();
+	}
 }
 
 
-namespace SocketClient
-{
-  class SocketClientProgram
-  {
-    public void Start()
-    {
-      try
-      {
-        Debug.Log("Попытка соединения");
-        byte[] bytes = new byte[1024];
-        IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse("10.0.1.46"), 8181);
-        Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+/// <summary>
+/// Класс в котором описывается соединение с сервером
+/// </summary>
+class SocketClientProgram
+	{
+		/// <summary>
+		/// Метод запускающий соединение. (синхронный сокет-клиент)
+		/// </summary>
+		public void Start ()
+		{
+		//попытка соединения с сервером.
+			try {
+				// размер буфера 
+				byte[] buffers = new byte[1024];
+				// конечная точка для подключения состоит из адреса и порта, данные беруться с Json файла
+				IPEndPoint ipEndPoint = new IPEndPoint (SettingsGame.ipServer, SettingsGame.portServer);
+				// Создание сокета по протоколу TCP\IP
+				Socket sender = new Socket (AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				Debug.Log ("Попытка соединения с - " + SettingsGame.ipServer + " порт " + SettingsGame.portServer);
+				
+				// попытка отправить сообщение VideoSystem
+				try {
+					//Соединяемся с сокетом
+					sender.Connect (ipEndPoint);
+					//Отправляем массив байт (ковертирование)
+					sender.Send (Encoding.ASCII.GetBytes ("VideoSystem"));
+					//НАчинаем получать входящие данные
+					while (true) {
+						// Записываетм данные в буфер
+						sender.Receive (buffers);
+						// Десереализируем структуру данных 
+						DataSt.Data _d = DataSt.Data.Deserialize (buffers);
+						// Выводим на экран данные (при билде программы необходимо поставить галочку Development Build)
+						Debug.LogError (_d.coordinate);
+						// Передаем данные скрипту управляющему локомотивом.
+						TrainController.CurentPosition = (float)_d.coordinate;
+						// очищаем буфер
+						buffers = new byte[1024];
+					}
+				} catch (Exception) {
 
-          try
-          {
-            sender.Connect(ipEndPoint);
-            sender.Send(Encoding.UTF8.GetBytes("<VideoSystem>"));
-            while(true)
-            {
-              int bytesRec = sender.Receive(bytes);
-              DataSt.Data _d = DataSt.Data.Deserialize(bytes);
-              Debug.Log(_d.currentSpeed);
-              TrainController.CurentPosition = (float)_d.currentSpeed;
-              bytes = new byte[1024];
-            }
-          }
-          catch(Exception)
-          {
-
-            Debug.Log("Exeptio");
-          }
-      }
-      catch(Exception ex)
-      {
-        Debug.Log("Exception");
-      }
-      finally
-      {
-        Debug.Log("FinalyBlog");
+					Debug.Log ("Exeptio");
+				}
+			} catch (Exception ex) {
+				Debug.Log ("Exception");
+			} finally {
+				Debug.Log ("FinalyBlog");
        
-      }
-    }
-  }
-}
+			}
+		}
+	}
+
--- Assets/_scripts/TrainController.cs
+++ Assets/_scripts/TrainController.cs
@@ -3,31 +3,36 @@ using System.Collections;
 
 public class TrainController : MonoBehaviour
 {
+	/// <summary>
+	/// Объект пустышка которые перемещается в координату
+	/// </summary>
+	public GameObject target;
+	/// <summary>
+	/// Текущая позиция
+	/// </summary>
+	public static float CurentPosition = 0;
 
-    public GameObject target;
-    public static float Speed { get; set; }
-    public float speed = 10;
-    public static float CurentPosition=0;
-  
-  
-  public void Update()
-  {
-   
-    target.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, CurentPosition);
-   // c();
-  }
+	/// <summary>
+	/// Метод выполняется каждый кадр отрисовка. Частота выполнения зависит от показателя FPS
+	/// </summary>
+	public void Update ()
+	{
+		target.transform.position = new Vector3 (target.transform.position.x, target.transform.position.y, CurentPosition);
+	//	FakeServerRequest (); //off
+	}
+	/// <summary>
+	/// Вызывается в конце кадра
+	/// </summary>
+	void LateUpdate ()
+	{
+		Camera.main.transform.position = Vector3.Lerp (Camera.main.transform.position, target.transform.position, Time.deltaTime * 50);
+	}
 
-  void LateUpdate()
-  {
-    Camera.main.transform.position = Vector3.MoveTowards(
-     Camera.main.transform.position, target.transform.position, Time.deltaTime * 100
-    );
-  }
-
-
-  void c()
-  {
-    CurentPosition += 0.1f;
-    ;
-  }
+	/// <summary>
+	/// Fakes the server request.
+	/// </summary>
+	void FakeServerRequest ()
+	{
+		CurentPosition += 2;
+	}
 }
